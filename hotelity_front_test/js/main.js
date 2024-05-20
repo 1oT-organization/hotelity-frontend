d7a372833e3d290defd66f0a85787e3d9c9ed665
seongmin <gam1205@naver.com>
2024-05-20 16:05:47 +0900

    Feat: "Front Template Test"

--- hotelity_front_test/js/main.js
+++ hotelity_front_test/js/main.js
@@ -58,22 +58,23 @@
         nav : false
     });
 
-    // Clock
-    const h1 = document.getElementById("time")
+ // Clock
+const h1 = document.getElementById("time");
 
-    let handleId = 0;
+function getTime() {
+    const date = new Date();
+    const hour = String(date.getHours()).padStart(2, '0');
+    const minute = String(date.getMinutes()).padStart(2, '0');
+    const second = String(date.getSeconds()).padStart(2, '0');
+    const time = `${hour}:${minute}:${second}`;
+    h1.textContent = time;
+}
 
-    function getTime(){
-        const date = new Date()
-        const hour = date.getHours();
-        const minute = date.getMinutes();
-        const second = date.getSeconds();
-        const time = `${hour}:${minute}:${second}`
-        setInterval(getTime, 1000);
-        h1.textContent = time;
-    }
+// 최초에 한 번 시간 설정
+getTime();
 
-    getTime();
+// 1초마다 getTime 함수를 호출하도록 타이머 설정
+setInterval(getTime, 1000);
     
 
 
