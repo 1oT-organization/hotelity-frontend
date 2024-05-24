1c55e9c8f667987db9aa292522c5850d25f54228
seongmin <gam1205@naver.com>
2024-05-24 16:43:37 +0900

    Init: "TEST"

--- hotelity/src/router/index.js
+++ hotelity/src/router/index.js
@@ -4,6 +4,7 @@ import CustomerList from '@/component/customer/CustomerList.vue';
 import EmployeeList from '@/component/employee/EmployeeList.vue';
 import Test from '@/component/Test.vue';
 import TestChart from '@/component/TestChart.vue';
+import EmployeeInfo from '@/component/employee/EmployeeInfo.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -27,6 +28,10 @@ const router = createRouter({
     {
       path: '/chartTest',
       component: TestChart
+    },
+    {
+      path: '/employeeInfo',
+      component: EmployeeInfo
     }
 ]
 });
