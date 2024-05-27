08c80c6d246bf544e9a829e180d958dac537a56b
bucky <null00007@naver.com>
2024-05-27 14:48:19 +0900

    Feat: "예약 화면 작성 중"

---
+++ hotelity/src/component/hotel-service/reservation/ReservationCalendar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app">
+        <CalendarComponent />
+    </div>
+</template>
+
+<script>
+
+import '@/assets/fullcalendar/index.global.js';
+
+    document.addEventListener('DOMContentLoaded', function () {
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+            initialView: 'dayGridMonth'
+        });
+        calendar.render();
+    });
+</script>
+
+<style>
+/* 스타일 추가 */
+</style>
--- hotelity/src/component/hotel-service/reservation/ReservationPage.vue
+++ hotelity/src/component/hotel-service/reservation/ReservationPage.vue
@@ -2,12 +2,16 @@
     <div>
         <ReservationSidevar />
         <ReservationNavbar />
+
+        <ReservationCalendar />
     </div>
 </template>
 
 <script setup>
 import ReservationNavbar from "@/component/hotel-service/reservation/ReservationNavbar.vue";
 import ReservationSidevar from "@/component/hotel-service/reservation/ReservationSidevar.vue";
+import ReservationCalendar from "@/component/hotel-service/reservation/ReservationCalendar.vue";
+
 </script>
 
 <style lang="scss" scoped></style>
