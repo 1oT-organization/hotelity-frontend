15eaea17bea39707b4371e71a3015abbb3707645
bucky <null00007@naver.com>
2024-05-30 10:50:27 +0900

    Feat: "투숙 리스트 조회 기능 구현"
    axios.get()을 사용한 조회 기능 구현 및 체크아웃용 체크박스 추가
    
    Resolves: #31

---
+++ hotelity/src/component/hotel-service/stay/StaySidebar.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="sidebar pe-4 pb-3">
+    <nav class="navbar bg-secondary navbar-dark">
+      <a href="index.html" class="navbar-brand mx-4 mb-3">
+        <h3 class="text-primary" style="display: flex; justify-content: center;"><img
+            src="../../../assets/img/hotelity_logo.png" width="60%"></h3>
+      </a>
+
+      <Clock/>
+
+      <div class="navbar-nav w-100">
+        <router-link to="/reservationPage" class="nav-item nav-link">
+          <i class="emoji bi bi-people-fill"></i>예약 일정
+        </router-link>
+        <router-link to="/stayPage" class="nav-item nav-link active">
+          <i class="emoji bi bi-person-fill-add"></i>투숙
+        </router-link>
+        <router-link to="/paymentPage" class="nav-item nav-link">
+          <i class="emoji bi bi-person-fill-add"></i>결제
+        </router-link>
+      </div>
+    </nav>
+
+  </div>
+
+</template>
+
+<script setup>
+import Clock from "@/component/common/Clock.vue";
+</script>
+
+<style>
+@import "@/css/style.css";
+@import "@/css/bootstrap.min.css";
+
+.dropdown-icon {
+  transition: transform 0.5s;
+}
+
+.filter-container {
+  display: none;
+  position: absolute;
+  top: 50px;
+  right: 10px;
+  width: 500px;
+  padding: 10px;
+  background-color: white;
+  border-radius: 5px;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+  display: flex;
+  gap: 10px;
+}
+
+.filter-container::before {
+  content: "";
+  position: absolute;
+  top: -10px;
+  right: 20px;
+  border-width: 0 10px 10px 10px;
+  border-style: solid;
+  border-color: transparent transparent white transparent;
+}
+
+.position-relative-container {
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+}
+
+.emoji {
+  margin-right: 10px;
+}
+</style>
---
+++ hotelity/src/component/hotel-service/stay/stayPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <StaySidebar />
+    <StayNavbar />
+  </div>
+</template>
+
+<script setup>
+
+import StaySidebar from "@/component/hotel-service/stay/StaySidebar.vue";
+import StayNavbar from "@/component/hotel-service/stay/StayNavbar.vue";
+</script>
+
+<style lang="scss" scoped>
+
+</style>
