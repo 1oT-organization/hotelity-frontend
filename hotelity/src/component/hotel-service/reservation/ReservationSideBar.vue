b047587343d0f258eb80a39aedffc42bbf2f6e60
bucky <null00007@naver.com>
2024-05-30 09:35:35 +0900

    Common: "clock 컴포넌트 common 폴더에 추가"
    공통 사용되는 clock 컴포넌트 common 폴더에 추가

--- hotelity/src/component/hotel-service/reservation/ReservationSideBar.vue
+++ hotelity/src/component/hotel-service/reservation/ReservationSideBar.vue
@@ -1,5 +1,4 @@
 <template>
-  <ReservationNavbar/>
 
   <div class="sidebar pe-4 pb-3">
     <nav class="navbar bg-secondary navbar-dark">
@@ -8,11 +7,12 @@
             src="../../../assets/img/hotelity_logo.png" width="60%"></h3>
       </a>
 
-      <div class="container">
-        <div class="clock">
-          <h1 id="time" style="display: flex; justify-content: center; font-family: fantasy; color:#798a69;"></h1>
-        </div>
-      </div>
+      <Clock/>
+<!--      <div class="container">-->
+<!--        <div class="clock">-->
+<!--          <h1 id="time" style="display: flex; justify-content: center; font-family: fantasy; color:#798a69;"></h1>-->
+<!--        </div>-->
+<!--      </div>-->
 
       <div class="navbar-nav w-100">
         <router-link to="/reservationPage" class="nav-item nav-link active">
@@ -32,41 +32,42 @@
 </template>
 
 <script setup>
+import Clock from "@/component/common/Clock.vue";
 import ReservationNavbar from "@/component/hotel-service/reservation/ReservationNavbar.vue";
-import {onMounted} from "vue";
-
-onMounted(() => {
-  fetchData().then(() => {
-    // isLoading.value = false;
-  });
-
-  function fetchData() {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve();
-      }, 1000);
-    });
-  }
-
-  // Clock
-  const h1 = document.getElementById("time");
-
-  function getTime() {
-    const date = new Date();
-    const hour = String(date.getHours()).padStart(2, '0');
-    const minute = String(date.getMinutes()).padStart(2, '0');
-    const second = String(date.getSeconds()).padStart(2, '0');
-    const time = `${hour}:${minute}:${second}`;
-    h1.textContent = time;
-  }
-
-// 최초에 한 번 시간 설정
-  getTime();
-
-// 1초마다 getTime 함수를 호출하도록 타이머 설정
-  setInterval(getTime, 1000);
-
-});
+// import {onMounted} from "vue";
+//
+// onMounted(() => {
+//   fetchData().then(() => {
+//     // isLoading.value = false;
+//   });
+//
+//   function fetchData() {
+//     return new Promise(resolve => {
+//       setTimeout(() => {
+//         resolve();
+//       }, 1000);
+//     });
+//   }
+//
+//   // Clock
+//   const h1 = document.getElementById("time");
+//
+//   function getTime() {
+//     const date = new Date();
+//     const hour = String(date.getHours()).padStart(2, '0');
+//     const minute = String(date.getMinutes()).padStart(2, '0');
+//     const second = String(date.getSeconds()).padStart(2, '0');
+//     const time = `${hour}:${minute}:${second}`;
+//     h1.textContent = time;
+//   }
+//
+// // 최초에 한 번 시간 설정
+//   getTime();
+//
+// // 1초마다 getTime 함수를 호출하도록 타이머 설정
+//   setInterval(getTime, 1000);
+//
+// });
 
 </script>
 
