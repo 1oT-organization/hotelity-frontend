778f79ad255410532139fee89046da8fd4159a74
jhlee <jun73045928@gmail.com>
2024-06-03 10:36:10 +0900

    Refactor: "전역 설정 파일 수정"
    CSS 포인터 추가
    불필요 코드 제거

--- hotelity/src/assets/js/combine.js
+++ hotelity/src/assets/js/combine.js
@@ -21,31 +21,6 @@
 //     }
 //   }
 
-document.addEventListener('DOMContentLoaded', function () {
-    const dropdownToggles = document.querySelectorAll('.dropdown-toggle');
-
-    dropdownToggles.forEach(toggle => {
-        toggle.addEventListener('click', function (event) {
-            event.stopPropagation(); 
-            
-            const icon = toggle.querySelector('.dropdown-icon');
-            if (icon) {
-                const isExpanded = toggle.getAttribute('aria-expanded') === 'true';
-                icon.style.transform = !isExpanded ? 'rotate(-180deg)' : 'rotate(0deg)';
-            }
-        });
-    });
-
-    document.addEventListener('click', function () {
-        dropdownToggles.forEach(toggle => {
-            const icon = toggle.querySelector('.dropdown-icon');
-            if (icon) {
-                icon.style.transform = 'rotate(0deg)';
-            }
-        });
-    });
-});
-
 $(document).ready(function() {
     $('#filter-icon').on('click', function() {
         $('.filter-container').toggle();
@@ -118,174 +93,173 @@ $(document).ready(function() {
     Chart.defaults.borderColor = "#000000";
 
 
-    var ctx1 = $("#worldwide-sales").get(0).getContext("2d");
-var myChart1 = new Chart(ctx1, {
-    type: "bar",
-    data: {
-        labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
-        datasets: [{
-                label: "USA",
-                data: [15, 30, 55, 65, 60, 80, 95],
-                backgroundColor: "rgba(235, 22, 22, .7)"
-            },
-            {
-                label: "UK",
-                data: [8, 35, 40, 60, 70, 55, 75],
-                backgroundColor: "rgba(235, 22, 22, .5)"
-            },
-            {
-                label: "AU",
-                data: [12, 25, 45, 55, 65, 70, 60],
-                backgroundColor: "rgba(235, 22, 22, .3)"
-            }
-        ]
-    },
-    options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        plugins: {
-            title: {
-                display: true,
-                text: 'Worldwide Sales'
-            }
-        }
-    }
-});
+    // var ctx1 = $("#worldwide-sales").get(0).getContext("2d");
+    // var myChart1 = new Chart(ctx1, {
+    //     type: "bar",
+    //     data: {
+    //         labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
+    //         datasets: [{
+    //                 label: "USA",
+    //                 data: [15, 30, 55, 65, 60, 80, 95],
+    //                 backgroundColor: "rgba(235, 22, 22, .7)"
+    //             },
+    //             {
+    //                 label: "UK",
+    //                 data: [8, 35, 40, 60, 70, 55, 75],
+    //                 backgroundColor: "rgba(235, 22, 22, .5)"
+    //             },
+    //             {
+    //                 label: "AU",
+    //                 data: [12, 25, 45, 55, 65, 70, 60],
+    //                 backgroundColor: "rgba(235, 22, 22, .3)"
+    //             }
+    //         ]
+    //     },
+    //     options: {
+    //         responsive: true,
+    //         maintainAspectRatio: false,
+    //         plugins: {
+    //             title: {
+    //                 display: true,
+    //                 text: 'Worldwide Sales'
+    //             }
+    //         }
+    //     }
+    // });
 
 
     // Salse & Revenue Chart
-    var ctx2 = $("#salse-revenue").get(0).getContext("2d");
-    var myChart2 = new Chart(ctx2, {
-        type: "line",
-        data: {
-            labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
-            datasets: [{
-                    label: "Salse",
-                    data: [15, 30, 55, 45, 70, 65, 85],
-                    backgroundColor: "rgba(235, 22, 22, .7)",
-                    fill: true
-                },
-                {
-                    label: "Revenue",
-                    data: [99, 135, 170, 130, 190, 180, 270],
-                    backgroundColor: "rgba(235, 22, 22, .5)",
-                    fill: true
-                }
-            ]
-            },
-        options: {
-            responsive: true
-        }
-    });
+    // var ctx2 = $("#salse-revenue").get(0).getContext("2d");
+    // var myChart2 = new Chart(ctx2, {
+    //     type: "line",
+    //     data: {
+    //         labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
+    //         datasets: [{
+    //                 label: "Salse",
+    //                 data: [15, 30, 55, 45, 70, 65, 85],
+    //                 backgroundColor: "rgba(235, 22, 22, .7)",
+    //                 fill: true
+    //             },
+    //             {
+    //                 label: "Revenue",
+    //                 data: [99, 135, 170, 130, 190, 180, 270],
+    //                 backgroundColor: "rgba(235, 22, 22, .5)",
+    //                 fill: true
+    //             }
+    //         ]
+    //         },
+    //     options: {
+    //         responsive: true
+    //     }
+    // });
     
 
-
     // Single Line Chart
-    var ctx3 = $("#line-chart").get(0).getContext("2d");
-    var myChart3 = new Chart(ctx3, {
-        type: "line",
-        data: {
-            labels: [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150],
-            datasets: [{
-                label: "Salse",
-                fill: false,
-                backgroundColor: "rgba(235, 22, 22, .7)",
-                data: [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15]
-            }]
-        },
-        options: {
-            responsive: true
-        }
-    });
+    // var ctx3 = $("#line-chart").get(0).getContext("2d");
+    // var myChart3 = new Chart(ctx3, {
+    //     type: "line",
+    //     data: {
+    //         labels: [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150],
+    //         datasets: [{
+    //             label: "Salse",
+    //             fill: false,
+    //             backgroundColor: "rgba(235, 22, 22, .7)",
+    //             data: [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15]
+    //         }]
+    //     },
+    //     options: {
+    //         responsive: true
+    //     }
+    // });
 
 
     // Single Bar Chart
-    var ctx4 = $("#bar-chart").get(0).getContext("2d");
-    var myChart4 = new Chart(ctx4, {
-        type: "bar",
-        data: {
-            labels: ["Italy", "France", "Spain", "USA", "Argentina"],
-            datasets: [{
-                backgroundColor: [
-                    "rgba(235, 22, 22, .7)",
-                    "rgba(235, 22, 22, .6)",
-                    "rgba(235, 22, 22, .5)",
-                    "rgba(235, 22, 22, .4)",
-                    "rgba(235, 22, 22, .3)"
-                ],
-                data: [55, 49, 44, 24, 15]
-            }]
-        },
-        options: {
-            responsive: true
-        }
-    });
+    // var ctx4 = $("#bar-chart").get(0).getContext("2d");
+    // var myChart4 = new Chart(ctx4, {
+    //     type: "bar",
+    //     data: {
+    //         labels: ["Italy", "France", "Spain", "USA", "Argentina"],
+    //         datasets: [{
+    //             backgroundColor: [
+    //                 "rgba(235, 22, 22, .7)",
+    //                 "rgba(235, 22, 22, .6)",
+    //                 "rgba(235, 22, 22, .5)",
+    //                 "rgba(235, 22, 22, .4)",
+    //                 "rgba(235, 22, 22, .3)"
+    //             ],
+    //             data: [55, 49, 44, 24, 15]
+    //         }]
+    //     },
+    //     options: {
+    //         responsive: true
+    //     }
+    // });
 
 
     // Pie Chart
-    var ctx5 = $("#pie-chart").get(0).getContext("2d");
-    var myChart5 = new Chart(ctx5, {
-        type: "pie",
-        data: {
-            labels: ["Italy", "France", "Spain", "USA", "Argentina"],
-            datasets: [{
-                backgroundColor: [
-                    "rgba(235, 22, 22, .7)",
-                    "rgba(235, 22, 22, .6)",
-                    "rgba(235, 22, 22, .5)",
-                    "rgba(235, 22, 22, .4)",
-                    "rgba(235, 22, 22, .3)"
-                ],
-                data: [55, 49, 44, 24, 15]
-            }]
-        },
-        options: {
-            responsive: true
-        }
-    });
+    // var ctx5 = $("#pie-chart").get(0).getContext("2d");
+    // var myChart5 = new Chart(ctx5, {
+    //     type: "pie",
+    //     data: {
+    //         labels: ["Italy", "France", "Spain", "USA", "Argentina"],
+    //         datasets: [{
+    //             backgroundColor: [
+    //                 "rgba(235, 22, 22, .7)",
+    //                 "rgba(235, 22, 22, .6)",
+    //                 "rgba(235, 22, 22, .5)",
+    //                 "rgba(235, 22, 22, .4)",
+    //                 "rgba(235, 22, 22, .3)"
+    //             ],
+    //             data: [55, 49, 44, 24, 15]
+    //         }]
+    //     },
+    //     options: {
+    //         responsive: true
+    //     }
+    // });
 
 
     // Doughnut Chart
-    var ctx6 = $("#doughnut-chart").get(0).getContext("2d");
-    var myChart6 = new Chart(ctx6, {
-        type: "doughnut",
-        data: {
-            labels: ["Italy", "France", "Spain", "USA", "Argentina"],
-            datasets: [{
-                backgroundColor: [
-                    "rgba(235, 22, 22, .7)",
-                    "rgba(235, 22, 22, .6)",
-                    "rgba(235, 22, 22, .5)",
-                    "rgba(235, 22, 22, .4)",
-                    "rgba(235, 22, 22, .3)"
-                ],
-                data: [55, 49, 44, 24, 15]
-            }]
-        },
-        options: {
-            responsive: true
-        }
-    });
+    // var ctx6 = $("#doughnut-chart").get(0).getContext("2d");
+    // var myChart6 = new Chart(ctx6, {
+    //     type: "doughnut",
+    //     data: {
+    //         labels: ["Italy", "France", "Spain", "USA", "Argentina"],
+    //         datasets: [{
+    //             backgroundColor: [
+    //                 "rgba(235, 22, 22, .7)",
+    //                 "rgba(235, 22, 22, .6)",
+    //                 "rgba(235, 22, 22, .5)",
+    //                 "rgba(235, 22, 22, .4)",
+    //                 "rgba(235, 22, 22, .3)"
+    //             ],
+    //             data: [55, 49, 44, 24, 15]
+    //         }]
+    //     },
+    //     options: {
+    //         responsive: true
+    //     }
+    // });
 
     
 })(jQuery);
 
 
  // Clock
-const h1 = document.getElementById("time");
-
-function getTime() {
-    const date = new Date();
-    const hour = String(date.getHours()).padStart(2, '0');
-    const minute = String(date.getMinutes()).padStart(2, '0');
-    const second = String(date.getSeconds()).padStart(2, '0');
-    const time = `${hour}:${minute}:${second}`;
-    h1.textContent = time;
-}
+// const h1 = document.getElementById("time");
+//
+// function getTime() {
+//     const date = new Date();
+//     const hour = String(date.getHours()).padStart(2, '0');
+//     const minute = String(date.getMinutes()).padStart(2, '0');
+//     const second = String(date.getSeconds()).padStart(2, '0');
+//     const time = `${hour}:${minute}:${second}`;
+//     h1.textContent = time;
+// }
 
 // 최초에 한 번 시간 설정
-getTime();
+// getTime();
 
 // 1초마다 getTime 함수를 호출하도록 타이머 설정
-setInterval(getTime, 1000);
+// setInterval(getTime, 1000);
 
